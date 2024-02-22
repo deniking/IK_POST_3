122a93aabb9ed93a2a9611f8dbb57d255d768886
DENIS <denikingya@gmail.com>
2024-02-22 20:02:53 +0200

    1

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+ghfghfgf
+hvhhhgfhfg
+hghfghfhgf
 
 
 def print_hi(name):
